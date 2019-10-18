841a2c907cf269ed7801f0ad132de46f947be959
Antonio Montes <antonio_montes@outlook.com>
2019-10-17 19:10:57 -0700

    Refactored animations

--- make_superoptimizer.bat
+++ make_superoptimizer.bat
@@ -1,2 +1,2 @@
 cd src
-C:\millfork-0.3.8\millfork.exe twin_dragons.mfk -o ..\bin\twin_dragons -t nes_small_v -O9
+C:\millfork-0.3.8\millfork.exe twin_dragons.mfk -i .\anims -o ..\bin\twin_dragons -t nes_small_v -O9
