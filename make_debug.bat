11a809e453c36d846e7db196d5f8784877e58b21
Antonio Montes <antonio_montes@outlook.com>
2019-10-17 23:35:56 -0700

    Added music

--- make_debug.bat
+++ make_debug.bat
@@ -1,2 +1,2 @@
 cd src
-C:\millfork-0.3.8\millfork.exe twin_dragons.mfk -i .\anims -o ..\bin\twin_dragons -t nes_small_v -G fceux -O1
+C:\millfork-0.3.8\millfork.exe twin_dragons.mfk -i .\anims -i .\music -o ..\bin\twin_dragons -t nes_small_v -G fceux -O1
