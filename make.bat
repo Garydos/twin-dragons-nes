d64da9d0cab837480f535cae4191afcf401af2ba
Antonio Montes <antonio_montes@outlook.com>
2019-10-23 20:07:11 -0700

    Updated volatile variables

--- make.bat
+++ make.bat
@@ -1,2 +1,2 @@
 cd src
-C:\millfork-0.3.8\millfork.exe twin_dragons.mfk -i .\anims -i .\music -o ..\bin\twin_dragons -t nes_small_v -O4
+java -jar C:\millfork-nightly\millfork.jar twin_dragons.mfk -i .\anims -i .\music -o ..\bin\twin_dragons -t nes_small_v -O4
