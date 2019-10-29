c18a3f00c7e593b3e5b59ee0ba98262f67df26cc
Antonio Montes <antonio_montes@outlook.com>
2019-10-28 20:22:03 -0700

    Refactored animations file

--- make_debug.bat
+++ make_debug.bat
@@ -1,2 +1,2 @@
 cd src
-java -jar C:\millfork-nightly\millfork.jar twin_dragons.mfk -i .\anims -i .\music -o ..\bin\twin_dragons -t nes_small_v -G fceux -O1
+C:\millfork-0.3.10\millfork.exe twin_dragons.mfk -i .\music -o ..\bin\twin_dragons -t nes_small_v -G fceux -O1
