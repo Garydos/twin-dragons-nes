646a23a81d1dc07094693995da679027c8fd4c58
Antonio Montes <antonio_montes@outlook.com>
2019-10-28 21:36:35 -0700

    Major refactoring, preparing for full level-loading system

--- make_debug.bat
+++ make_debug.bat
@@ -1,2 +1,2 @@
 cd src
-C:\millfork-0.3.10\millfork.exe twin_dragons.mfk -i .\music -o ..\bin\twin_dragons -t nes_small_v -G fceux -O1
+C:\millfork-0.3.10\millfork.exe twin_dragons.mfk -i .\music -i .\levels -o ..\bin\twin_dragons -t nes_small_v -G fceux -O1
