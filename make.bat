343eaed9eaaff2b260595cc5d8012a4c335a25c8
Antonio Montes <antonio_montes@outlook.com>
2019-10-28 21:38:57 -0700

    Refactored main file

--- make.bat
+++ make.bat
@@ -1,2 +1,2 @@
 cd src
-C:\millfork-0.3.10\millfork.exe twin_dragons.mfk -i .\music -i .\levels -o ..\bin\twin_dragons -t nes_small_v -O4
+C:\millfork-0.3.10\millfork.exe main.mfk -i .\music -i .\levels -o ..\bin\twin_dragons -t nes_small_v -O4
