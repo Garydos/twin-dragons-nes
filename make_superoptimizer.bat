4c5f5ea6f98084007cb32ddd3ec7928838b05cce
Antonio Montes <antonio_montes@outlook.com>
2019-11-10 19:44:09 -0800

    Refactoring + hid sprite0

--- make_superoptimizer.bat
+++ make_superoptimizer.bat
@@ -1,2 +1,2 @@
 cd src
-C:\millfork-0.3.10\millfork.exe main.mfk -i .\music -i .\levels -o ..\bin\twin_dragons -t nes_small_v -O9
+C:\millfork-0.3.10\millfork.exe main.mfk -i .\music -i .\levels -i .\enemies -o ..\bin\twin_dragons -t nes_small_v -O9
