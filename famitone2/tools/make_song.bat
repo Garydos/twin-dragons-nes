d64fcb2e18b180cb7b02555c970ab11020450718
Antonio Montes <antonio_montes@outlook.com>
2019-11-13 12:19:07 -0800

    Updated Music

--- famitone2/tools/make_song.bat
+++ famitone2/tools/make_song.bat
@@ -5,7 +5,7 @@ set /p dpcm_addr="Enter the base address of your DPCM samples (listed as FT_DPCM
 IF "%dpcm_addr%"=="" (
     SET dpcm_addr=0xfc00
 )
-set /p base_addr="Enter starting address for your music data (where it will be placed in ROM)[default=0xcac0]: "
+set /p base_addr="Enter starting address for your music data (where it will be placed in ROM)[default=0xc5c0]: "
 IF "%base_addr%"=="" (
     SET base_addr=0xc5c0
 )
