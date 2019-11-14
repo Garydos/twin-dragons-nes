ed41ab05b1e0a7030c2ccfbc534a77b8fa811054
Antonio Montes <antonio_montes@outlook.com>
2019-11-14 15:08:55 -0800

    Prepared new enemy animations

--- make_debug.bat
+++ make_debug.bat
@@ -1,2 +1,2 @@
 cd src
-C:\millfork-0.3.10\millfork.exe main.mfk -i .\music -i .\levels -i .\enemies -o ..\bin\twin_dragons -t nes_small_v -G fceux -O1
+C:\millfork-0.3.12\millfork.exe main.mfk -i .\music -i .\levels -i .\enemies -o ..\bin\twin_dragons -t nes_small_v -G fceux -O1
